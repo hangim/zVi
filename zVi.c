8fe2d10ef75ce8f019f8c048dc55a8a29eeea707
pdszhh <pdszhh@qq.com>
2014-06-04 14:35:49 +0800

    destory string

--- zVi.c
+++ zVi.c
@@ -8,44 +8,21 @@ struct String {
     struct String *next;
 };
 
-struct String * create_String_node(char const *buf) {
-    struct String *s = (struct String *) malloc(sizeof(struct String));
-    
-    if (buf == NULL) { // head node
-        s->s = NULL;
-    } else {
-        s->s = (char *) malloc(STRING_MAX_SIZE + 1);
-        strncpy(s->s, buf, STRING_MAX_SIZE);
-        s->s[STRING_MAX_SIZE] = '\0';
-    }
-    
-    s->next = NULL;
-
-    return s;
-}
-
-struct String * create_String (char const *buf) {
-    if (buf == NULL)
-        return NULL;
-
-    int len = strlen(buf);
-    struct String *head, *p;
-    head = p = create_String_node(NULL);
-
-    while (len > 0) {
-        p->next = create_String_node(buf);
-        len -= STRING_MAX_SIZE;
-        buf += STRING_MAX_SIZE;
-        p = p->next;
-    }
-
-    return head;
-}
-
+void String_test();
+struct String * String_create_node(char const *buf);
+struct String * String_create (char const *buf);
+void String_destory (struct String * string);
 
 
 int main(int argc, char const *argv[]) {
 
+    String_test();
+    
+    return 0;
+}
+
+
+void String_test() {
     char buf[] = "hello world " "hello world " "hello world " "hello world " "hello world\n"
         "hello world " "hello world " "hello world " "hello world " "hello world\n"
         "hello world " "hello world " "hello world " "hello world " "hello world\n"
@@ -57,12 +34,65 @@ int main(int argc, char const *argv[]) {
         "hello world " "hello world " "hello world " "hello world " "hello world\n"
         "hello world " "hello world " "hello world " "hello world " "hello world\n";
 
-    struct String *s = create_String(buf);
+    struct String *s, *p;
+    s = p = String_create(buf);
 
-    while (s->next) {
-        printf("%s", s->next->s);
-        s = s->next;
+    while (p->next) {
+        printf("%s", p->next->s);
+        p = p->next;
     }
 
-    return 0;
+    String_destory(s);
+    s = p = NULL;
+}
+
+struct String * String_create_node(char const *buf) {
+    struct String *s = (struct String *) malloc(sizeof(struct String));
+    if (s == NULL)
+        ; // TODO
+    
+    if (buf == NULL) { // head node
+        s->s = NULL;
+    } else {
+        s->s = (char *) malloc(STRING_MAX_SIZE + 1);
+        if (s->s == NULL)
+            ; // TODO
+        strncpy(s->s, buf, STRING_MAX_SIZE);
+        s->s[STRING_MAX_SIZE] = '\0';
+    }
+    
+    s->next = NULL;
+
+    return s;
+}
+
+struct String * String_create (char const *buf) {
+    if (buf == NULL)
+        return NULL;
+
+    int len = strlen(buf);
+    struct String *head, *p;
+    head = p = String_create_node(NULL);
+
+    while (len > 0) {
+        p->next = String_create_node(buf);
+        len -= STRING_MAX_SIZE;
+        buf += STRING_MAX_SIZE;
+        p = p->next;
+    }
+
+    return head;
+}
+
+void String_destory (struct String * string) {
+    if (string == NULL)
+        return;
+
+    struct String  *p, *q;
+    p = q = string;
+    while (p) {
+        q = q->next;
+        free(p);
+        p = q;
+    }
 }
