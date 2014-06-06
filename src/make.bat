32278291d5b7c9a7887bed3e4159cf4d804fe2fa
pdszhh <pdszhh@qq.com>
2014-06-06 10:14:10 +0800

    fuck GBK

--- src/make.bat
+++ src/make.bat
@@ -1,10 +1,10 @@
 @echo off
 
-gcc string.c    -c -std=c99 -Wall
-gcc line.c      -c -std=c99 -Wall
-gcc view.c      -c -std=c99 -Wall
-gcc zVi.c -c    -std=c99 -Wall
+gcc string.c -c -std=c99 -Wall -fexec-charset=GBK
+gcc line.c   -c -std=c99 -Wall -fexec-charset=GBK
+gcc view.c   -c -std=c99 -Wall -fexec-charset=GBK
+gcc zVi.c    -c -std=c99 -Wall -fexec-charset=GBK
 
-gcc string.o line.o view.o zVi.o -o zVi -std=c99 -Wall
+gcc string.o line.o view.o zVi.o -o zVi -std=c99 -Wall -fexec-charset=GBK
 
 del string.o line.o view.o zVi.o
